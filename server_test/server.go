34b7b9ab80f8ca1204cfd1ebd92444bb8fb3e2b2
mvigneras <maxence.vigneras@gmail.com>
2018-08-10 10:19:17 -0400

    server displays images
    images are now displayed with a star if they are selected

--- server_test/server.go
+++ server_test/server.go
@@ -5,21 +5,35 @@ import (
 	"net/http"
 	"os"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"log"
 	"io/ioutil"
 	"bitbucket.org/avcl/pik"
 	"bytes"
+	"github.com/labstack/echo/middleware"
 )
 
 func upload(c echo.Context) error {
-	//name := c.FormValue("name")
-	//email := c.FormValue("email")
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+
+	if name == "" {
+		return c.HTML(http.StatusOK, "<body>Please enter a valid name</body>")
+		panic("error")
+	}
+
+	if email == "" {
+		return c.HTML(http.StatusOK, "<body>Please enter a valid email</body>")
+		panic("error")
+	}
+
+	New_Dir_Name := "name="+name+"-"+"email="+email
+	os.Mkdir("public/"+New_Dir_Name,0777)
 
 	form, err := c.MultipartForm()
 	if err != nil {
 		return err
 	}
+
 	files := form.File["files"]
 
 	for _, file := range files {
@@ -28,8 +42,8 @@ func upload(c echo.Context) error {
 			return err
 		}
 		defer src.Close()
-
-		dst, err := os.Create(file.Filename)
+		//need to create all imgs in the new dir above
+		dst, err := os.Create("./public/"+New_Dir_Name+"/"+file.Filename)
 		if err != nil {
 			return err
 		}
@@ -40,19 +54,21 @@ func upload(c echo.Context) error {
 		}
 
 	}
-	image_list := pickcl()
+	image_list := pickcl(New_Dir_Name)
 	var str bytes.Buffer
 	for _, l := range image_list{
 		str.WriteString(l)
 	}
+	//os.RemoveAll("./public/"+New_Dir_Name)
 	return c.HTML(http.StatusOK, string(str.String()))
 }
 
-func pickcl() []string{
-	root := "./"
+func pickcl(New_Dir_Name string) []string{
+	root := "./public/"+New_Dir_Name+"/"
 	pf := photoFactory{}
 	contextId := pik.NewDesktopContext()
 	files, err := ioutil.ReadDir(root)
+
 	if err != nil{
 		log.Fatal(err)
 	}
@@ -72,9 +88,12 @@ func pickcl() []string{
 		b, _ := pik.IsSelected(contextId, f.relativePath)
 		//fix to display HTML Image with a star next to it after it is printable
 		if b {
-			image_list = append(image_list, f.Name+"is_selected, ")
+			image_list = append(image_list, "image "+f.Name+":    ")
+			image_list = append(image_list, "<p><img src='"+New_Dir_Name+"/"+f.Name+"' style='width:100px;height:100px;'>")
+			image_list = append(image_list, "<img src='Star.jpg' style='width:100px;height:100px;'></p>")
 		} else {
-			image_list = append(image_list, f.Name+"is_not_selected, ")
+			image_list = append(image_list, "image "+f.Name+":    ")
+			image_list = append(image_list, "<p><img src='"+New_Dir_Name+"/"+f.Name+"' style='width:100px;height:100px;'></p>")
 		}
 	}
 	return image_list
